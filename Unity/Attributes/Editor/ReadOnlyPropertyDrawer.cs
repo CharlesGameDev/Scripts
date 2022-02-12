1b5a3fea8d3220e71d1ba8f9529f7cf31c0bd2de
CharlesGameDev <99226468+CharlesGameDev@users.noreply.github.com>
2022-02-11 23:42:45 -0500

    Create ReadOnlyPropertyDrawer.cs

---
+++ Unity/Attributes/Editor/ReadOnlyPropertyDrawer.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+public class ReadOnlyPropertyDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        var previousGUIState = GUI.enabled;
+        GUI.enabled = false;
+        EditorGUI.PropertyField(position, property, label);
+        GUI.enabled = previousGUIState;
+    }
+}
